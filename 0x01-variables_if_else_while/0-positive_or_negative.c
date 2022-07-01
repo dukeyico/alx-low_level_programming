dbc5f1164f6a809cce14f62e1a12c7534c6363cb
Duke Okojie <dukeosamhengbe@gmail.com>
2022-07-01 16:12:56 +0100

    Conditional loop programming

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,13 +9,12 @@
 int main(void)
 {
 		int n, last;
-
 		srand(time(0));
 		n = rand() - RAND_MAX / 2;
 		last = n % 10;
 		if (last > 5)
 		{
-			printf("%d is positive\n", n);
+		    printf("%d is positive\n", n);
 		}
 		else if (last < 6 && last != 0)
 		{

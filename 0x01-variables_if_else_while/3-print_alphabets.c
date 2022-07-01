cd99522eefe4f246d75263bbfd5807c1c903264f
Duke Okojie <dukeosamhengbe@gmail.com>
2022-07-01 17:49:45 +0100

    conditional loop programming

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,17 +6,16 @@
  */
 int main(void)
 {
-	char c = 'a';
-	int i;
-
-	for (i = 0; i < 26; i++)
-	{
-		putchar(c + i);
-	}
-	for (i = 0; i < 26; i++)
-	{
-		putchar((c + i) - 32);
-	}
-	putchar(10);
-	return (0);
+char c = 'a';
+int i;
+for (i = 0; i < 26; i++)
+{
+putchar(c + i);
+}
+for (i = 0; i < 26; i++)
+{
+putchar((c + i) - 32);
+}
+putchar(10);
+return (0);
 }

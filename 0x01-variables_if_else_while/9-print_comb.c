31327b10b518e0a36b9fa6213402bf7ce855b92d
dukeyico <dukeosamhengbe@gmail.com>
2022-07-01 23:25:14 -0700

    conditional loop programming

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,17 +6,17 @@
  */
 int main(void)
 {
-	int i = 0;
+int i = 0;
 
-	for (i = 0; i < 10; i++)
-	{
-		putchar(i + 48);
-		if (i < 9)
-		{
-			putchar(44);
-			putchar(32);
-		}
-	}
-	putchar(10);
-	return (0);
+for (i = 0; i < 10; i++)
+{
+putchar(i + 48);
+if (i < 9)
+{
+putchar(44);
+putchar(32);
+}
+}
+putchar(10);
+return (0);
 }

e856b89b5c6d6c4469c622058bd7b3210091002d
dukeyico <dukeosamhengbe@gmail.com>
2022-07-06 00:01:59 -0700

    prints alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,11 +6,11 @@
  */
 void print_alphabet(void)
 {
-char letter = 'a';
-while (letter <= 'z')
+char letter = a;
+while (letter <= z)
 {
 _putchar(letter);
 letter++;
 }
-_putchar('\n');
+_putchar(n);
 }

534245221ee85fdf1412ed2a6a93a82f24b65e59
dukeyico <dukeosamhengbe@gmail.com>
2022-07-14 06:29:22 -0700

    pointers and arrays

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,7 +9,7 @@ char *rot13(char *s)
 int count = 0, i;
 char alphabet[53] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-while (*(s + count) != '\0')
+while (*(s + count) != 0)
 {
 for (i = 0; i < 52; i++)
 {

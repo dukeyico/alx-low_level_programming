c786f28a8dae0b79feae20ce83ff42d23936866e
dukeyico <dukeosamhengbe@gmail.com>
2022-07-28 05:55:25 -0700

    more malloc and free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
+#include main.h
 /**
  * is_digit - checks if a string contains a non-digit char
  * @s: string to be evaluated
@@ -38,7 +38,7 @@ return (i);
  */
 void errors(void)
 {
-printf("Error\n");
+printf(Errorn);
 exit(98);
 }
 /**
@@ -53,13 +53,11 @@ int main(int argc, char *argv[])
 char *s1, *s2;
 int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 s1 = argv[1], s2 = argv[2];
-if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 errors();
 len1 = _strlen(s1);
 len2 = _strlen(s2);
 len = len1 + len2 + 1;
 result = malloc(sizeof(int) * len);
-if (!result)
 return (1);
 for (i = 0; i <= len1 + len2; i++)
 result[i] = 0;
@@ -84,7 +82,6 @@ a = 1;
 if (a)
 _putchar(result[i] + '0');
 }
-if (!a)
 _putchar('0');
 _putchar('\n');
 free(result);
